72d5f33a8c7f285e12f2b4e28700e678550b420b
Nicole <yh397@cornell.edu>
2025-05-23 02:45:26 -0400

    fix: add missing newline at end of index.css and add comments in main.tsx for clarity

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// find an HTML element with the ID "root" in the DOM
+// 
 createRoot(document.getElementById("root")!).render(<App />);
